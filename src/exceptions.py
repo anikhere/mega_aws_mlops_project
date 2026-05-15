48814c53d12b4ffc4bc3a2c6a1f7612e86501065
tahaanik <tahaanik729@gmail.com>
2026-05-15 22:12:56 +0530

    completed eda

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,13 +1,22 @@
 import sys
-class CustomException:
-    def __init__(self,error_msg,error_detail:sys):
-        super.__init__(error_msg)
-        _,_,exec_tb = error_detail.exc_info()
-        self.file_name = exec_tb.tb_frame.f_code.co_filename
-        self.file_no = exec_tb.tb_lineno
+from src.logger import logger
+
+class CustomException(Exception):
+    def __init__(self, error_msg, error_detail: sys):
+        super().__init__(error_msg)
+        _, _, exc_tb = error_detail.exc_info()
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.file_no = exc_tb.tb_lineno
 
     def __str__(self):
-        print(f'the error occured in file{self.file_name}')
-        print(f'having the line_no:=={self.file_no}')
-        print(f'having the message of{self.args[0]}')
-        
+        return (f'Error in file: {self.file_name}, '
+                f'line: {self.file_no}, '
+                f'message: {self.args[0]}')
+
+if __name__ == "__main__":
+    try:
+        value = 10/0
+        print(value)
+    except Exception as e:
+        logger.info('cant print this')
+        raise CustomException(str(e), sys)
--- src/logger.py
+++ src/logger.py
@@ -3,10 +3,10 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path = os.path.join(os.getcwd,'logs',LOG_FILE)
-os.makedirs(os.path.dirname(log_path))
+log_path = os.path.join(os.getcwd(),'logs',LOG_FILE)
+os.makedirs(os.path.dirname(log_path),exist_ok=True)
 logging.basicConfig(
-    filename=LOG_FILE,
+    filename=log_path,
     format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO
 )
